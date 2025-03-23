ced93b8e374c747a1017e8707a73d099a389adef
Prado3 <lukasprado8@gmail.com>
2025-03-23 18:57:30 -0300

    Imports

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,6 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe {
-    
+    uint256 public minimumUsd = 5;
+
+    function fund() public payable {
+        //0x694AA1769357215DE4FAC081bf1f309aDC325306
+        require(msg.value >= minimumUsd,"didn t send enough ETH");
+    }
+
+    function getPrice() public view returns (uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (,int256 price,,,) = priceFeed.latestRoundData();
+        return uint256(price * 1e18);
+    }
+
+    function getConversionRate() public {
+
+    }
+
+    function getVersion() public view returns (uint256){
+        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+    }
 }
