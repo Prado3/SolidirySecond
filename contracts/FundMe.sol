4ae298573f52df70d7965fd65615c250370407a2
Prado3 <lukasprado8@gmail.com>
2025-03-24 19:33:22 -0300

    Avance

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,34 +1,23 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.0;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./contracts/PriceConverter.sol";
 
 contract FundMe {
+    using PriceConverter for uint256;
     uint256 public minimumUsd = 5 * 1e18;
 
     address[] public funders;
-    mapping(address founder => uint256 amountFunded) public addressToAmountFunded;
+    mapping(address => uint256) public addressToAmountFunded;
+
 
     function fund() public payable {
+        
         //0x694AA1769357215DE4FAC081bf1f309aDC325306
-        require(getConversionRate(msg.value) >= minimumUsd,"didn t send enough ETH");
+        require(msg.value.getConversionRate() >= minimumUsd,"didn t send enough ETH");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
     }
 
-    function getPrice() public view returns (uint256){
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (,int256 price,,,) = priceFeed.latestRoundData();
-        return uint256(price * 1e18);
-    }
-
-    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountUsd = (ethPrice * ethAmount) / 1e18;
-        return ethAmountUsd;
-    }
-
-    function getVersion() public view returns (uint256){
-        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
-    }
+    
 }
---
+++ contracts/PriceConverter.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+library PriceConvert {
+    function getPrice() internal  view returns (uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (,int256 price,,,) = priceFeed.latestRoundData();
+        return uint256(price * 1e18);
+    }
+
+    function getConversionRate(uint256 ethAmount) internal  view returns(uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountUsd;
+    }
+
+    function getVersion() internal  view returns (uint256){
+        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+    }
+}
