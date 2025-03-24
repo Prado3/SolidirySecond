eadf3b1cde6455c183a38f0ba464f5cbdd87ae9c
Prado3 <lukasprado8@gmail.com>
2025-03-24 19:20:08 -0300

    FundMe1

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -4,11 +4,16 @@ pragma solidity ^0.8.18;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
-    uint256 public minimumUsd = 5;
+    uint256 public minimumUsd = 5 * 1e18;
+
+    address[] public funders;
+    mapping(address founder => uint256 amountFunded) public addressToAmountFunded;
 
     function fund() public payable {
         //0x694AA1769357215DE4FAC081bf1f309aDC325306
-        require(msg.value >= minimumUsd,"didn t send enough ETH");
+        require(getConversionRate(msg.value) >= minimumUsd,"didn t send enough ETH");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
     }
 
     function getPrice() public view returns (uint256){
@@ -17,8 +22,10 @@ contract FundMe {
         return uint256(price * 1e18);
     }
 
-    function getConversionRate() public {
-
+    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountUsd;
     }
 
     function getVersion() public view returns (uint256){
